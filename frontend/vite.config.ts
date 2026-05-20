7481c024a4cc50b65bd8b456cf89608076046b86
ANAND PANDEY <anandpandey20005@gmail.com>
2026-05-20 16:25:56 +0530

    prerender or sitemap uninstall

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,30 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import prerender from '@prerenderer/rollup-plugin'
-import sitemap from 'vite-sitemap'
-
-const prerenderRoutes = ['/', '/experience', '/project']
-const sitemapRoutes = ['experience', 'project']
 
 export default defineConfig({
 
   plugins: [
     react(),
     tailwindcss(),
-    prerender({
-      routes: prerenderRoutes,
-      renderer: '@prerenderer/renderer-puppeteer',
-      rendererOptions: {
-        headless: true,
-      }
-    }),
-    sitemap({
 
-      base: 'https://yourwebsite.com',
-      urls: sitemapRoutes,
-      changefreq: 'weekly',
-      robotsTxt: 'User-agent: *\nAllow: /'
-    })
-  ],
+  ]
 })
